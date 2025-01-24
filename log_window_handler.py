0be2764961cc55aa5795e2c65462ed2afef6cf3d
ityeri <yeongbin0909@naver.com>
2025-01-24 10:26:04 +0900

    여러줄 출력할수 있게 수정

--- log_window_handler.py
+++ log_window_handler.py
@@ -15,9 +15,22 @@ class LogWindowHandler(logging.Handler):
     # 로그 올라갈때 뜨는거
     def emit(self, record: logging.LogRecord):
         dt = datetime.datetime.fromtimestamp(record.created)
-        message = f"[{dt.strftime('%Y-%m-%d %H:%M:%S')}] [{record.levelname}] : {record.msg}"
 
-        self.logWindow.addstr(message + '\n')
+        for line in record.msg.split("\n"):
+            self.logWindow.addstr(f"[{dt.strftime('%Y-%m-%d %H:%M:%S')}] ", curses.color_pair(10))
+            self.logWindow.addstr(f"[{record.levelname}] ", curses.color_pair(11))
+
+            color = None
+
+            match record.levelno:
+                case logging.INFO: color = curses.color_pair(1)
+                case logging.WARN: color = curses.color_pair(2)
+                case logging.ERROR: color = curses.color_pair(3)
+                case logging.DEBUG: color = curses.color_pair(4)
+
+            self.logWindow.addstr(f": {line}", color)
+            self.logWindow.addstr("\n")
+
         self.logWindow.refresh()
 
         self.moveCurserTo(0, 0)
